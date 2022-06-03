7faf1a29a7af7f3b4e194090f0acd7eedbd2be80
Faelin Landy <faelin@umich.edu>
2022-06-03 17:03:09 -0400

    lotto guess

--- some_script.sh
+++ some_script.sh
@@ -24,3 +24,17 @@ do
   ((count++))
 done
 
+
+
+echo "Enter your lucky number"
+read n
+case $n in
+101)
+  echo echo "You got 1st prize" ;;
+510)
+  echo "You got 2nd prize" ;;
+999)
+  echo "You got 3rd prize" ;;
+*)
+  echo "Sorry, try for the next time" ;;
+esac
