cf4748a42f15329b315fafcf4e18f7c10865ebe3
Faelin Landy <faelin@umich.edu>
2022-06-03 17:01:53 -0400

    add mroe script

--- some_script.sh
+++ some_script.sh
@@ -4,4 +4,9 @@ echo "Printing text with newline"
 echo -n "Printing text without newline"
 echo -e "\nRemoving \t backslash \t characters\n"
 
+# Add two numeric value
+((sum=25+35))
+
+#Print the result
+echo $sum
 
